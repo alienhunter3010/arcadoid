c63c3518f928511ca182d4cc31e9f6867a41d37c
Alessio Cecchin <acecchin@gmail.com>
2019-06-06 01:24:00 +0200

    The ball is bouncing

--- game/actives.py
+++ game/actives.py
@@ -7,6 +7,9 @@ class Vaus(LiveItem):
     def __init__(self, bounds=(0,)):
         super().__init__('vaus-static.png', position=(400, 550), speed=(12, 0), bounds=bounds)
 
+    def bounceEffect(self, ball):
+        ball.onXBounce()
+        
     def events(self, pressed):
         if self.freeze:
             return
@@ -19,6 +22,39 @@ class Vaus(LiveItem):
 
     def event(self, event):
         pass
+
+    def output(self, screen):
+        self.render(screen, Sprite.update(self))
+
+
+class Ball(LiveItem):
+    NNE = (2, -4)
+    ENE = (4, -2)
+    NNO = (-2, -4)
+    ONO = (-4, -2)
+    SSE = (2, 4)
+    ESE = (4, 2)
+    SSO = (-2, 4)
+    OSO = (-4, 2)
+
+    def __init__(self, bounds=(0,)):
+        super().__init__('ball.png', position=(414, 446), speed=[2, -4], bounds=bounds)
+
+    def get_shape(self):
+        return pygame.Rect(self.position, (self.image.get_width(), self.image.get_height()))
+
+    def onXBounce(self):
+        self.speed[0] *= -1
+
+    def onYBounce(self, absolute = False):
+        if absolute:
+            self.speed[1] = abs(self.speed[1])
+        self.speed[1] *= -1
+
+    def run(self):
+        dx = self.speed[0]
+        dy = self.speed[1]
+        self.move(dx, dy)
 
     def output(self, screen):
         self.render(screen, Sprite.update(self))
--- game/arena.py
+++ game/arena.py
@@ -1,10 +1,12 @@
-from game.actives import Vaus
+import pygame
+from game.actives import Vaus, Ball
 
 
 class Arena:
     def __init__(self, chart):
         self.chart = chart
         self.player = Vaus(bounds=(20,)).moveOn(self.chart)
+        self.ball = Ball(bounds=(20,)).moveOn(self.chart)
 
     def ProcessInput(self, events, pressed_keys):
         self.player.events(pressed_keys)
@@ -12,6 +14,14 @@ class Arena:
     def Render(self, screen):
         screen.fill((20, 20, 20))
         self.player.output(screen)
+        self.ball.output(screen)
+
+        shape_ball = self.emboss(self.ball.get_shape())
+        if self.player.get_rect().colliderect(shape_ball):
+            self.player.bounceEffect(self.ball)
 
     def Update(self):
-        pass
+        self.ball.run()
+
+    def emboss(self, rect):
+        return pygame.Rect((rect.left -1, rect.top -1), (rect.width +2, rect.height +2))
